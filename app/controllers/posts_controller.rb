2e7e283e5a17de8c106d9977dbdb7ad3b1fc3755
kiprotichdominic <kiprotichdominic102@gmail.com>
2022-10-02 10:28:48 +0300

    seed sample data to db

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,23 @@
 class PostsController < ApplicationController
+  skip_before_action :authenticate_request, only: [:create,:index,:show]
+  before_action :set_post, only: [:show, :destroy]
+
+  def index
+    render json: Post.all, status: :ok
+  end
+  def show
+    render json: post, status: :ok
+  end
+  def create
+  end
+
+  private
+
+  def post_params
+    params.permit(:title,:content)
+  end
+
+  def set_post
+    post = Post.find(params[:id])
+  end
 end
